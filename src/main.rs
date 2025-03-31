f9d47d179bd6b2fb91f3f7e32a0aa41c47edaad0
Chongbing Yu <bd8ejk@foxmail.com>
2025-03-31 09:17:01 +0800

    feat(sensor): add `AsyncUartSensorDriver`
    Signed-off-by: Chongbing Yu <bd8ejk@foxmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+#[allow(unused_imports)]
 use esp_idf_svc as svc;
 use hal::sys;
 use svc::hal;
 
-use esp32s3_demo::sensor::{I2cSensorDriver, UartSensorDriver};
+use esp32s3_demo::sensor::{AsyncUartSensorDriver, I2cSensorDriver, UartSensorDriver};
 use hal::delay::FreeRtos;
 use hal::gpio::AnyIOPin;
 use hal::i2c::{I2cConfig, I2cDriver};
 use hal::peripherals::Peripherals;
 use hal::prelude::*;
-use hal::uart::{self, UartDriver};
+use hal::task::block_on;
+use hal::uart::{self, AsyncUartDriver, UartDriver};
 
 fn main() -> anyhow::Result<()> {
     sys::link_patches();
@@ -22,7 +24,7 @@ fn main() -> anyhow::Result<()> {
     let uart1_rx = p.pins.gpio11;
 
     let uart1_config = uart::config::Config::default().baudrate(Hertz(115_200));
-    let uart1 = UartDriver::new(
+    let uart1 = AsyncUartDriver::new(
         p.uart1,
         uart1_tx,
         uart1_rx,
@@ -30,7 +32,7 @@ fn main() -> anyhow::Result<()> {
         Option::<AnyIOPin>::None,
         &uart1_config,
     )?;
-    let mut uart1_sensor = UartSensorDriver::new(uart1);
+    let mut uart1_sensor = AsyncUartSensorDriver::new(uart1);
 
     let i2c0_cfg = I2cConfig::new()
         .baudrate(400.kHz().into())
@@ -39,18 +41,18 @@ fn main() -> anyhow::Result<()> {
     let i2c0 = I2cDriver::new(p.i2c1, i2c0_sda, i2c0_scl, &i2c0_cfg)?;
     let mut i2c0_sensor = I2cSensorDriver::new(i2c0);
 
-    loop {
-        let angle = &mut [0 as f32; 3];
-        i2c0_sensor.read_angle(angle)?;
+    block_on(async {
+        loop {
+            let angle = i2c0_sensor.read_angle()?;
 
-        let distance = &mut [0 as u16; 1];
-        uart1_sensor.read_distance(distance)?;
+            let distance = uart1_sensor.read_distance().await?;
 
-        log::info!("Distance: {}mm", distance[0]);
-        log::info!("Roll: {:.2}", angle[0]);
-        log::info!("Pitch: {:.2}", angle[1]);
-        log::info!("Yaw: {:.2}", angle[2]);
+            log::info!("Distance: {}mm", distance);
+            log::info!("Roll: {:.2}", angle[0]);
+            log::info!("Pitch: {:.2}", angle[1]);
+            log::info!("Yaw: {:.2}", angle[2]);
 
-        FreeRtos::delay_ms(2000);
-    }
+            FreeRtos::delay_ms(2000);
+        }
+    })
 }
